af2cbb0e6be7c37c5693e876aefde893699f5b9a
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-05-06 01:22:43 +0300

    Small fixes

---
+++ Niwa.Database/Migrations/20240505221437_EmptyMigrationToFixPasswordConsistency.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Niwa.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmptyMigrationToFixPasswordConsistency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("aaf91a62-1964-46c6-ab36-a95af1486272"),
+                column: "PasswordHash",
+                value: "SuAXex8b0XkT/edfOK4u1qQfqSNVKlBeLqfk1ZAHOsg=;gsBfnorqA0irnJd155F5Mw==");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("aaf91a62-1964-46c6-ab36-a95af1486272"),
+                column: "PasswordHash",
+                value: "TOtQd+OOYtLwvvwdwph3ovZn9Bz2sCsQ26CG/keH/0g=;po//Xdg4nknQnWe/8XDM3A==");
+        }
+    }
+}
